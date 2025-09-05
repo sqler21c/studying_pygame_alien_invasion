31f6616ecc2bdd02320a86c131dbe4c9d5dac873
sun_main <sqler21c@gmail.com>
2025-09-05 16:16:24 +0900

    chapter 13-5 complete

---
+++ src/exam/exam_chaper13/13-5/settings.py
@@ -0,0 +1,24 @@
+class Settings:
+    """A class to store all settings for Sideways Shooter."""
+    def __init__(self):
+        """Initialize the game's settings."""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+
+        # ship settings
+        self.ship_speed = 3.0
+
+        # bullet settings
+        self.bullet_speed = 6.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 3
+
+        # alien settinng 
+        self.alien_frequency = 0.008
+        self.alien_speed = 1.5
+
+        
