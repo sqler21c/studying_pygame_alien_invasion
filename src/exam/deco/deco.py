b5210d363278e30e1fe424684e53fd04ffb8bfa5
sun_main <sqler21c@gmail.com>
2025-09-08 15:48:25 +0900

    deco 추가

--- src/exam/deco/deco.py
+++ src/exam/deco/deco.py
@@ -47,3 +47,57 @@ def big_number2(n):
 big_mumber1(3)
 print("big 2")
 big_number2(6)
+
+
+print("==========")
+
+def function(a, b):
+    print("funtion 실행")
+    return a + b
+
+print(function(1, 3))
+print(function.__name__)
+
+func = function
+func(10, 210)
+
+def check_func_name(func):
+    print("함수 이름 : ", func.__name__)
+
+check_func_name(func)
+
+
+
+def deco(func):
+    def new_func(*args, **kwargs):
+        print('deco 함수 ')
+        result = func(*args, **kwargs)
+        return result
+    return new_func
+
+
+@deco
+def add_a_b(a, b):
+    return a + b
+
+print(add_a_b(10, 20))
+check_func_name(add_a_b)
+
+from functools import wraps
+
+def decor(func):
+    @wraps(func)
+    def new_funco(*args, **kwargs):
+        print("wwaps 적용한 데코함수")
+        result = func(*args, **kwargs)
+        return result
+    return new_funco
+
+@decor
+def add_a_b_1(a, b):
+    return a + b
+
+
+print(add_a_b_1(10, 20))
+check_func_name(add_a_b_1)
+
